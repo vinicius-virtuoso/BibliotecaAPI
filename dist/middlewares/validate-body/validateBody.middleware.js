c4d2bacb621322f06a59451c234085734ed77047
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2023-05-22 20:15:52 -0300

    refac: ajuste do deploy 7

--- dist/middlewares/validate-body/validateBody.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateBody = void 0;
-const validateBody = (schema) => async (req, res, next) => {
-    const validation = schema.parse(req.body);
-    req.body = validation;
-    return next();
-};
-exports.validateBody = validateBody;
--- dist/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRouter = void 0;
-const express_1 = require("express");
-const middlewares_1 = require("../middlewares");
-const schemas_1 = require("../schemas");
-const login_controller_1 = require("../controllers/login/login.controller");
-exports.loginRouter = (0, express_1.Router)();
-exports.loginRouter.post('/', (0, middlewares_1.validateBody)(schemas_1.loginSchema), login_controller_1.loginController.login);
